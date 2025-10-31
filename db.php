b79708b3088ff8e8f0f9727d82e3710c609fae60
kimdaaeul0406-lang <kimdaaeul0406@gmail.com>
2025-10-31 14:28:22 +0900

    db변경

--- db.php
+++ db.php
@@ -4,9 +4,9 @@ function db(){
   if($pdo) return $pdo;
 
   $host = 'localhost';
-  $db   = 'shim_on';      // schema.sql로 만든 DB 이름
-  $user = 'root';         // ★ XAMPP 기본
-  $pass = '';             // ★ 비밀번호 없음(빈 문자열)
+  $db   = 'daseul0406';      // schema.sql로 만든 DB 이름
+  $user = 'daseul0406';         // ★ XAMPP 기본
+  $pass = 'a583400S@';             // ★ 비밀번호 없음(빈 문자열)
   $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
   $pdo = new PDO($dsn, $user, $pass, [
